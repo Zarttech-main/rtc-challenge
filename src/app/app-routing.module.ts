7b1c06b2da7506bf59cba0d3e862f4dc34e9c903
Chaluchukwu <chaliman.co@gmail.com>
2023-08-27 05:54:51 +0100

    added template for new meeting form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NewMeetingComponent } from './pages/new-meeting/new-meeting.component';
 
 const routes: Routes = [{
   path: "",  title: "Welcome", component: HomeComponent
+},{
+  path: "new-meeting",  title: "New Meeting", component: NewMeetingComponent
 },];
 
 @NgModule({
---
+++ src/app/pages/new-meeting/new-meeting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-meeting',
+  templateUrl: './new-meeting.component.html',
+  styleUrls: ['./new-meeting.component.css']
+})
+export class NewMeetingComponent {
+
+}
